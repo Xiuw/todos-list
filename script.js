b4b86a23a4104c5acd283ba6948f27d0b288a638
xiu wang <sylvi.xw@gmail.com>
2019-03-13 17:37:39 -0400

    'commit'

--- script.js
+++ script.js
@@ -1,119 +1,52 @@
-
-
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var close = document.getElementsByClassName("close");
-//create var list and select "ul" - itemToggle() 
-var list=document.querySelector("ul");
- 
-// another way to get select "class" 
-//var close1 = document.querySelector(".close");
-
-var completeUl=document.getElementById("completeUl");
-
+var delbtn = document.getElementsByClassName("delbtn");
 
 function setColor(){
-
-	// alert("You are typing");
-	input.setAttribute("class", "box");
+	// when typing box(style) is applied to userInput in html page 
+	input.setAttribute("class", "box"); 
 }
-
 
 //return the length of the input value
 function inputLength() {
 	return input.value.length;
 }
 
-//create tasks
+//create todo task and delete element
 function createListElement() {
-	if(inputValue === ""){
-  		alert("Yea, I'm inside the If");
-  		document.getElementById("message").textContent +="Please add your task";
-  	}	
-
-
+	
+  	//create li element, and add inputValue to the list and append under ul at index 0
 	var li = document.createElement("li");
 	var inputValue=document.createTextNode(input.value);
 	li.appendChild(inputValue);
 	ul.insertBefore(li, ul.childNodes[0]);
-	input.value = "";
-	// input.setAttribute("class","undoBox")
-
-
+	input.value = "";//reset the input value to empty after each insertion
 	
 	//create an delete button to the already exist list
 	var span = document.createElement("SPAN");
   	var txt = document.createTextNode("Delete");
-  	span.className = "close";
+  	span.className = "delbtn";
   	span.appendChild(txt);
   	li.appendChild(span);
 
-  
-
-
-
-  	//delete the item from the list
-  	for (i = 0; i < close.length; i++) {
-	close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-      //alert("1");
-
+ 
+  	//delete the parent element of delbtn from the list
+  	for (i = 0; i < delbtn.length; i++) {
+	delbtn[i].onclick = function() {
+      this.parentElement.style.display = "none";
     }
   }
-
-
 }
-
-
-
-
-/*****************
-This no need if you have empty list
-*****************/
-
-var myList = document.getElementsByTagName("LI");
-
-	for(var i = 0; i < myList.length; i++){
-		var span = document.createElement("SPAN");
-		var txt = document.createTextNode("Delete");
-		span.className="close";
-		span.appendChild(txt);
-		myList[i].appendChild(span);
-		
-	}
-
-
-
-
-for(var i=0; i< close.length; i++){
-
-	close[i].onclick=function(){
-		var div = this.parentElement;
-		div.style.display="none";
-
-			//alert("2");
-	}
-
-}
-
-
-
-/*****************
-End here
-*****************/
-
-
-
-
-
+//For click
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
+	}else{
+		alert('Your input is empty')
 	}
 }
-
+//For keypress
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
@@ -121,39 +54,20 @@ function addListAfterKeypress(event) {
 }
 
 
-
-
-//function for crossout and undo the item from to do list
+//toggle between 'crosseout' and 'undo crossout'
 function itemToggle(e){
 
 	if(e.target.tagName === "LI"){
 		e.target.classList.toggle("done");
-
-		
-
-	// 	var completeLi = document.createElement("li");
-	// 	var completeEls=document.createTextNode(e.target);
-	
-	// completeLi.appendChild(completeEls);
-	// completeUl.insertBefore(completeLi, .childNodes[0]);
-
-
 	} 
-	// console.log(this.innerText);
-
 }
-
-
-
-
-
 
 // addEvenListenr
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
 input.addEventListener("input", setColor);
-list.addEventListener("click", itemToggle);
-// close[i].addEventListener("click",delItem);
+ul.addEventListener("click", itemToggle);
+
 
 
 
